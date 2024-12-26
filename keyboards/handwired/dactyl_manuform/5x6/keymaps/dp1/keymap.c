8630571580e9c9aa877f9f09ab5c700e56587589
Mauricio Cruz <mauriciorc@mimica.ai>
2024-12-26 07:43:18 -0300

    removing right side of _GAME

--- keyboards/handwired/dactyl_manuform/5x6/keymaps/dp1/keymap.c
+++ keyboards/handwired/dactyl_manuform/5x6/keymaps/dp1/keymap.c
@@ -873,11 +873,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_GAME] = LAYOUT_5x6(
-   _______ , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                            KC_6   , KC_7   , KC_8   , KC_9   , KC_0   ,XXXXXXX ,
-   KC_T    , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                            KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   ,_______ ,
-   KC_G    , KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   ,                            KC_G   , KC_H   , KC_J   , KC_K   , KC_L   ,_______ ,
-   KC_B    , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   ,                            KC_B   , KC_N   , KC_M   ,KC_COMM , KC_DOT ,_______ ,
-                     KC_LALT ,KC_ESC ,                                                               _______ ,_______ ,
+   KC_ESC  , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   ,                            XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
+   KC_T    , KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   ,                            XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
+   KC_G    , KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   ,                            XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
+   KC_B    , KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   ,                            XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,XXXXXXX ,_______ ,
+                     KC_LALT ,XXXXXXX ,                                                               _______ ,_______ ,
                                                KC_SPACE, KC_ENT ,         _______ , _______ ,
                                                _______ ,_______ ,         _______ , _______ ,
                                                _______ ,_______ ,         _______ , _______
